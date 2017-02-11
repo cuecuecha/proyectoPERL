fa44043206bd03b7114447fbd7536ac1be367a04
Jorge Cuecuecha <hdzcuecuecha@gmail.com>
2017-02-10 21:38:15 -0600

    Menu listo

---
+++ pro3.pl
@@ -0,0 +1,82 @@
+#!perl
+use warnings;
+use strict;
+
+
+
+my $op;
+my $op2;
+my %menuP = (                           
+    help =>  sub{print"Indica una opción valida\n"},
+    1    =>  \&conf,
+    2   =>  \&act,
+    3   =>  sub { exit }, 
+);
+my %menuS = (                           
+    help =>  sub{print"Indica una opción valida\n"},
+    1    =>  \&dionaea,
+    2   =>  \&crowrie,
+    3   => \&red,
+    4   =>  \&menu, 
+);
+
+sub menu{  
+  system("clear"); #limpia la pantalla
+  while(1){
+    print"1) Configurar parámetros del sensor\n";
+    print"2) Consultar los parámetros actuales\n";
+    print"3) Salir\n";
+
+
+    print"Que deseas hacer:\n";
+    $op=<STDIN>;
+    chomp($op);
+
+    if(!$menuP{$op})
+    { 
+      warn "Comando desconocido: `$op'; Prueba 'help' la siguiente vez\n"; 
+    }
+    else
+    {
+      $menuP{$op}->($op);
+    }
+  }
+}
+
+sub conf{
+  system("clear");
+  while(1){
+    print"1) Configurar Dionaea\n";
+    print"2) Configurar Cowrie\n";
+    print"3) Configurar Red\n";
+    print"4) Salir\n";
+
+
+  print"Que deseas hacer:\n";
+  $op2=<STDIN>;
+  chomp($op2);
+
+  if(!$menuS{$op2})
+  { 
+    warn "Comando desconocido: '$op2'; Prueba 'help' la siguiente vez\n"; 
+  }
+  else
+  {
+    $menuS{$op2}->($op2);
+  }
+}
+}
+sub act{
+  print"configurar";
+}
+sub dionaea{
+   print"configurar";
+}
+sub crowrie{
+   print"configurar";
+}
+sub red{
+  print"configurar";
+}
+
+&menu;
