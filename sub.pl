b0d7489ffeec62f4819e02440cb48887ecec2a3a
arturo <arturo@localhost.localdomain>
2017-02-12 09:47:03 -0600

    Valida format de direccion ip

--- sub.pl
+++ sub.pl
@@ -3,15 +3,18 @@ use warnings;
 use strict;
 
 print ("Ingrese su IP");
-print &ip;
-
-sub ip{
-
 my $entradaIP=<STDIN>;
-	if ($entradaIP=~ m/\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/)
+print "No es una direccion ip valida" if (! &ipValida($entradaIP));
+
+=pod  ipValida recibe como parametro una cadena y dice si es una direccion ip valida.
+=cut
+#
+# Recibe una cadena que debe tener solo una direccion ip.
+# Si la cadena tiene cualquier cosa que no sea una ip regresa 0;
+sub ipValida{
+    if ($_[0]=~ /^\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b$/)
 	{
 		return 1;
-
 	}
 	else {
 		return 0;
